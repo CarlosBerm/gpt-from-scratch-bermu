e2dfdc58f2912445bc5deedd05d64e4dc2f4e696
Yuchen Wang <wangyuch@umich.edu>
2025-09-24 17:47:31 -0400

    Attention in progress

--- models/attention.py
+++ models/attention.py
@@ -7,13 +7,31 @@ import torch.nn.functional as F
 class Attention(nn.Module):
     def __init__(self, d_model):
         super().__init__()
-        pass
+
+        self.W_q =
+        self.W_k =
+        self.W_v =
+
+        self.row_dim = 1
+        self.col_dim = 2
 
     def forward(self, encodings_q, encodings_k, encodings_v, mask=None):
         """
         Apply the attention calculation, with an optional mask of booleans"""
 
-        # TODO: Implement
+         # encodings_*: (batch, seq_len, d_model)
+        q = 
+        k = 
+        v = 
+
+        # (batch, seq_len, d_model) @ (batch, d_model, seq_len) -> (batch, seq_len, seq_len)
+
+        if mask is not None:
+            # Mask logic
+            
+        attention_percents =
+
+        return
 
 
 class MultiHeadAttention:
