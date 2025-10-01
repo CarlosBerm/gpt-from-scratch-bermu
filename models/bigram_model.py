eebc47e6ae82026ee281d8fd6489a6cd00954125
Carlos Bermudez <carlos.bermudez@newgencode.com>
2025-10-01 18:41:35 -0400

    Generate Letters

--- models/bigram_model.py
+++ models/bigram_model.py
@@ -1,30 +1,27 @@
 import torch
+import string
 import torch.nn as nn
-from torch.nn import functional as F
+import torch.nn.functional as F
 
 class BigramLanguageModel(nn.Module):
-
     def __init__(self, vocab_size):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
-        # On embedding: https://stackoverflow.com/questions/44881999/word-embedding-lookuptable-word-embedding-visualizations
-
+    
     def forward(self, idx):
         """
-        idx -> (B,T)
-        targets -> (B,T)"""
+        idx -> (B, T)
+        targets -> (B, T)
+        """
 
-        logits = self.token_embedding_table(idx) # (B,T,C)
+        logits = self.token_embedding_table(idx) # (B, T, C)
         return logits
 
-    def configure_optimizer(self):
-        return torch.optim.AdamW(self.parameters(), lr=1e-3)
-    
-    def training_step(self, input, target):
-        logits = self.token_embedding_table(input)
+    def training_step(self, logits):
         B, T, C = logits.shape
         logits = logits.view(B*T, C)
-        targets = target.view(B*T)
+        targets = targets.view(B*T)
+
         loss = F.cross_entropy(logits, targets)
 
         return loss
@@ -39,3 +36,20 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
 
         return idx
+    
+vocab_size = 65
+
+model = BigramLanguageModel(vocab_size)
+wordsList = model.generate(torch.zeros((1, 1), dtype=torch.long), max_new_tokens=1000).tolist()
+
+alphabet = list(string.ascii_lowercase + string.ascii_uppercase)
+letter_to_num = {letter: idx for idx, letter in enumerate(alphabet)}
+
+num_to_letter = {idx: letter for letter, idx in letter_to_num.items()}
+
+matched_letters = []
+for num in wordsList[0]:
+    letter = num_to_letter.get(num, '?')
+    matched_letters.append(letter)
+
+print("Generated letters:", matched_letters)
